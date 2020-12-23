af9c8d2fddd0d9e95d165cd1510d52bdb247cd4b
MilhamAt <M477ashiddiq@gmail.com>
2020-12-23 22:39:12 +0700

    Initial Commit

---
+++ UIScrollSnapkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIScrollSnapkit
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 23/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
